697c6a63251cb999fd83b1fb12c5bb2b64b00e47
iskowan <112644138+iskowan@users.noreply.github.com>
2023-02-28 17:41:32 -0500

    Initial Commit

--- TemperatureSliderConverter/ViewController.swift
+++ TemperatureSliderConverter/ViewController.swift
@@ -9,6 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var celsiusLabel: UILabel!
+    
+    @IBOutlet var fahrenheitLabel: UILabel!
+    
+    @IBOutlet var slider: UISlider!{
+        didSet {
+            slider.maximumValue = 100
+            slider.minimumValue = -100
+            slider.value = 0
+        }
+    }
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        let tempratureCelcius = Int(round(sender.value))
+        celsiusLabel.text = "\(tempratureCelcius) °C"
+        let tempratureFahrenheit = Int(round((sender.value * 9/5) + 32))
+        fahrenheitLabel.text = "\(tempratureFahrenheit)°F"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
